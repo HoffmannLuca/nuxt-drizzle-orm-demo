850abbf91a83b2fe63d6e92bb256539146cccd9d
HoffmannLuca <luca.hoffmann1000@gmail.com>
2024-05-05 09:52:29 +0200

    fix css for project list and delete buttons

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -14,10 +14,10 @@
 <template>
   <div class="flex flex-col gap-3">
     <NuxtLink to="/projects/create" class="bg-green-500 hover:bg-green-400 px-2 py-1 text-white rounded mx-auto">Create New Project</NuxtLink>
-    <div class="flex justify-evenly">
+    <div class="flex">
       <DataPreview class="w-1/3" :data="projects"></DataPreview>
-      <div v-if="projects.length" class="flex flex-col gap-4">
-        <button class="bg-red-500 p-2 rounded" v-for="project in projects" @click="deleteProject(project.id)">delete - {{ project.id }}</button>
+      <div v-if="projects.length" class="flex flex-col gap-4 w-2/3">
+        <button class="bg-red-500 p-2 rounded w-fit mx-auto" v-for="project in projects" @click="deleteProject(project.id)">delete - {{ project.id }}</button>
       </div>
     </div>
   </div>
