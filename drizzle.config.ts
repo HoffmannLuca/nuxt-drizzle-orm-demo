96f7cb9dbd3da962efba1ec0da006b3aeaa3e897
HoffmannLuca <luca.hoffmann1000@gmail.com>
2024-05-05 09:54:34 +0200

    set db credentials as env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,10 +5,10 @@ export default {
   out: "./database/migration",
   driver: 'pg',
   dbCredentials: {
-    user: "root",
-    password: "root",
-    host: "127.0.0.1",
-    port: 5432,
-    database: "dev-db",
+    database: String(process.env.DB_NAME),
+    host: String(process.env.DB_HOST),
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   }
 } satisfies Config;
