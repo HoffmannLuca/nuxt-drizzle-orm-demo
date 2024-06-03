73035071e00e3501164d5b5d983471ad208e9434
HoffmannLuca <luca.hoffmann1000@gmail.com>
2024-06-04 00:17:29 +0200

    remove old migrations and schema

--- database/schema/project.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
-
-export const projects = pgTable('projects', {
-  id: serial('id').primaryKey(),
-  title: text('title'),
-  description: text('description'),
-});
